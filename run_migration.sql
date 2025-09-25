ef1809a3a87dd97b703be162247522eac4cfd5b9
House Design Project <house.design@example.com>
2025-09-25 14:38:25 +0700

    update

--- run_migration.sql
+++
@@ -1,9 +0,0 @@
--- Run this script to add the missing home_content table
--- Execute this in your PostgreSQL database
-
--- Run the home_content migration
-\i database/migrations/003_create_home_content_table.sql
-
--- Verify the table was created
-SELECT COUNT(*) FROM home_content;
-SELECT * FROM home_content LIMIT 1;
